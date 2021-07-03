08945ea962947e3fcd29456d48e65bac4171180a
Zahid Rabbi <alzahidrabbi@gmail.com>
2021-07-03 14:01:08 +0600

    delete operation successful

--- src/AllUser.js
+++ src/AllUser.js
@@ -1,41 +1,46 @@
-import React, {useEffect, useState} from 'react';
-import './AllUser.css'
+import React, { useEffect, useState } from "react";
+import "./AllUser.css";
 
 function AllUser() {
+  const [users, setUsers] = useState([]);
 
-    const [users, setUsers] = useState([]);
+  console.log(users);
 
-    console.log(users)
+  const deleteProduct = (id) => {
+    fetch(`http://localhost:5000/delete/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("DELETED SUCCESSFULLY YAHOO!...", data);
+      });
+  };
 
-    useEffect(() =>{
-        fetch('http://localhost:5000/users')
-            .then(res => res.json())
-            .then(data => {
-                setUsers(data);
-            })
-    },[])
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  }, []);
 
-    return (
-        <div>
-            <h2>
-                All User...
-            </h2>
-            <div className="">
-                {
-                    users.map(user=> 
-                        <div className="user">
-                            <p>{user.name}</p> 
-                            <p>{user.user}</p> 
-                            <p>{user.email}</p> 
-                            <p>{user.phone}</p>
-                            <button>edit</button>
-                            <button>Delete</button>
-                        </div>                                               
-                    )
-                }
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <h2>All User...</h2>
+      <div className="">
+        {users.map((user) => (
+          <div key={user._id} className="user">
+            <p>{user.name}</p>
+            <p>{user.user}</p>
+            <p>{user.email}</p>
+            <p>{user.phone}</p>
+            <button>edit</button>
+            <button onClick={() => deleteProduct(user._id)}>Delete</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default AllUser
+export default AllUser;
